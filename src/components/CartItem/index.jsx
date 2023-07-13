f64d18c5d45b88b9ee9119f13b6d38d806482199
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-07-13 11:29:30 -0300

    carrito funcional

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,9 +1,32 @@
 import { Image, ListGroup } from "react-bootstrap"
 import PropTypes from 'prop-types'
+import useCart from "../../hooks/useCart"
+import { types } from "../../types"
 
 
 export const CartItem = ({ drink }) => {
-  const { strDrink, strDrinkThumb, price } = drink
+  const { strDrink, strDrinkThumb, price, quantity } = drink
+  const { dispatch } = useCart()
+
+  const handleAddItem = () => {
+    dispatch({
+      type: types.addItemToCart,
+      payload: drink
+    })
+  }
+  const handleRemoveItem = () => {
+    dispatch({
+      type: types.removeItemFromCart,
+      payload: drink
+    })
+  }
+  const handleRemoveAllItem = ()=>{
+    dispatch({
+      type: types.removeAllItemsFromCart,
+      payload: drink
+    })
+  }
+
   return (
     <ListGroup.Item className="d-flex gap-2">
       <Image src={strDrinkThumb} width={"80px"} />
@@ -11,7 +34,24 @@ export const CartItem = ({ drink }) => {
         <h5>{strDrink}</h5>
         <hr />
         <div className="d-flex justify-content-between">
-          ${price}
+          ${price * quantity}
+          <div className="d-flex gap-2 align-items-center">
+            <button className="btn btn-sm btn-danger" onClick={handleRemoveItem}>
+              <i className="fa-solid fa-minus"></i>
+            </button>
+            <input
+              type="text"
+              style={{ width: "50px" }}
+              className="form-control"
+              value={quantity}
+            />
+            <button className="btn btn-sm btn-success" onClick={handleAddItem}>
+              <i className="fa-solid fa-plus"></i>
+            </button>
+            <button className=" btn btn-sm btn-danger" onClick={handleRemoveAllItem} >
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
         </div>
 
       </div>
@@ -25,7 +65,8 @@ CartItem.propTypes = {
   drink: PropTypes.shape({
     strDrink: PropTypes.string,
     strDrinkThumb: PropTypes.string,
-    price: PropTypes.number
+    price: PropTypes.number,
+    quantity: PropTypes.number
   })
 
 
--- src/reducers/cartReducer.jsx
+++ src/reducers/cartReducer.jsx
@@ -1,15 +1,47 @@
 import { types } from "../types";
 
 export const cartReducer = (state = [], action) => {
+    const {idDrink} = action.payload
+    const item = state.find((item) => item.idDrink === idDrink);
     switch (action.type) {
         case types.addItemToCart:
 
-            return [...state, action.payload];
+            return item
+                ?
+                state.map((item) =>
+                 item.idDrink === idDrink
+                    ? {
+                        ...item,
+                        quantity: item.quantity + 1
+                    }
+                    :
+                    item
+                )
+                :
+                [
+                    ...state,
+                    {
+                        ...action.payload,
+                        quantity: 1,
+                    }
+                ]
+
+
+                ;
         case types.removeItemFromCart:
-            return state.filter((item) => item.idDrink !== action.payload);
-            
+            return action.payload.quantity > 1
+                ? state.map((item) =>
+                    item.idDrink === idDrink
+                        ? {
+                            ...item,
+                            quantity: item.quantity - 1,
+                        }
+                        : item
+                )
+                :
+                state.filter((item) => item.idDrink !== idDrink);
         case types.removeAllItemsFromCart:
-            return state.filter((item) => item.idDrink !== action.payload);
+            return state.filter((item) => item.idDrink !== idDrink);
         default:
             return state;
     }
