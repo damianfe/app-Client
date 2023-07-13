65491a31e4018b68381b2d53f04876dff7f0900e
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-07-13 09:58:41 -0300

    cart item corregido

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,19 +1,32 @@
-import { ListGroup } from "react-bootstrap"
+import { Image, ListGroup } from "react-bootstrap"
 import PropTypes from 'prop-types'
 
 
 export const CartItem = ({ drink }) => {
-  const { strDrink } = drink
+  const { strDrink, strDrinkThumb, price } = drink
   return (
-    <ListGroup.Item>{strDrink}</ListGroup.Item>
+    <ListGroup.Item className="d-flex gap-2">
+      <Image src={strDrinkThumb} width={"80px"} />
+      <div style={{ width: "100%" }}>
+        <h5>{strDrink}</h5>
+        <hr />
+        <div className="d-flex justify-content-between">
+          ${price}
+        </div>
+
+      </div>
+
+    </ListGroup.Item>
   )
 }
 
 
 CartItem.propTypes = {
-drink : PropTypes.shape({
-  strDrink : PropTypes.string
-})
+  drink: PropTypes.shape({
+    strDrink: PropTypes.string,
+    strDrinkThumb: PropTypes.string,
+    price: PropTypes.number
+  })
 
-  
+
 }
--- src/services/drinks.service.js
+++ src/services/drinks.service.js
@@ -1,26 +1,28 @@
 import axios from 'axios'
 
- const apiURL= import.meta.env.VITE_API_URL
+const apiURL = import.meta.env.VITE_API_URL
 
 
- export const filterDrinksService = async (ingredient,category) => {
-try{
-    const url = `${apiURL}filter.php?i=${ingredient}&c=${category}`
-    const {data} = await axios.get(url)
-return data.drinks || []
-}catch (error){
-    throw new Error("Hubo un error al obtener la bebida")
+export const filterDrinksService = async (ingredient, category) => {
+    try {
+        const url = `${apiURL}filter.php?i=${ingredient}&c=${category}`
+        const { data } = await axios.get(url)
+        const drinks = data.drinks.map(drink => ({ ...drink, price: +((drink.idDrink / 10).toFixed(0)) }))
+
+        return drinks || []
+    } catch (error) {
+        throw new Error("Hubo un error al obtener la bebida")
+    }
+
 }
 
- }
-
- export const getRecipeService = async (drinkId) =>{
+export const getRecipeService = async (drinkId) => {
     try {
         const url = `${apiURL}lookup.php?i=${drinkId}`
-        const {data} = await axios.get(url)
-    return data.drinks[0] || []
+        const { data } = await axios.get(url)
+        return data.drinks[0] || []
     } catch (error) {
-        throw new Error("Hubo un error al obtener la receta") 
+        throw new Error("Hubo un error al obtener la receta")
     }
- }
+}
 
